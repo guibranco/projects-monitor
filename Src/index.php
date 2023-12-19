23a4607786f230c686f5a57ca824977524fbcc2b
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-12-19 13:13:02 +0000

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -4,13 +4,18 @@ require_once("config.php");
 
 ?>
 
-<style>
-body {
-  background: rgb(13, 17, 23)
-}
-</style>
+<html>
+  <head>
+    <title>Projects Monitor</title>
+    <style>
+      body { background: rgb(13, 17, 23) }
+    </style>
+  </head>
 
-<img alt="" src="https://github-readme-stats-guibranco.vercel.app/api?username=guibranco&line_height=28&card_width=490&hide_title=true&hide_border=true&show_icons=true&theme=chartreuse-dark&icon_color=7FFF00&include_all_commits=true&count_private=true&show=reviews,discussions_started&count_private=true" />
-<img alt="" src="https://github-readme-stats-guibranco.vercel.app/api/top-langs?hide_border=true&username=guibranco&include_all_commits=true&count_private=true&show_icons=true&theme=chartreuse-dark&layout=compact&langs_count=12" />
-<br />
-<img alt="" src="https://github-readme-streak-stats-flax.vercel.app/?user=guibranco&theme=github-green-purple&fire=FF6600" />
+  <body>
+    <img alt="" src="https://github-readme-stats-guibranco.vercel.app/api?username=guibranco&line_height=28&card_width=490&hide_title=true&hide_border=true&show_icons=true&theme=chartreuse-dark&icon_color=7FFF00&include_all_commits=true&count_private=true&show=reviews,discussions_started&count_private=true" />
+    <img alt="" src="https://github-readme-stats-guibranco.vercel.app/api/top-langs?hide_border=true&username=guibranco&include_all_commits=true&count_private=true&show_icons=true&theme=chartreuse-dark&layout=compact&langs_count=12" />
+    <br />
+    <img alt="" src="https://github-readme-streak-stats-flax.vercel.app/?user=guibranco&theme=github-green-purple&fire=FF6600" />
+  </body>
+</html>

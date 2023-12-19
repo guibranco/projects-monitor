69a17aeac8e7230d98302036eac4bd04460d54c8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-12-19 21:10:45 +0000

    Update index.php (#40)

--- Src/index.php
+++ Src/index.php
@@ -14,8 +14,6 @@ require_once("config.php");
 
   <body>
     <img alt="" src="https://github-readme-stats-guibranco.vercel.app/api?username=guibranco&line_height=28&card_width=490&hide_title=true&hide_border=true&show_icons=true&theme=chartreuse-dark&icon_color=7FFF00&include_all_commits=true&count_private=true&show=reviews,discussions_started&count_private=true" />
-    <img alt="" src="https://github-readme-stats-guibranco.vercel.app/api/top-langs?hide_border=true&username=guibranco&include_all_commits=true&count_private=true&show_icons=true&theme=chartreuse-dark&layout=compact&langs_count=12" />
-    <br />
     <img alt="" src="https://github-readme-streak-stats-flax.vercel.app/?user=guibranco&theme=github-green-purple&fire=FF6600" />
   </body>
 </html>

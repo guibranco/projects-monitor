e47f8126a8ae4eb019118238b1cbbc387fd614ee
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-02-24 23:25:39 +0000

    Add timeout

--- Src/index.php
+++ Src/index.php
@@ -65,6 +65,8 @@ require_once("config.php");
       pieChart.draw(dataEvents, optionsEvents);
       var table = new google.visualization.Table(document.getElementById('table_div'));
       table.draw(dataFeed, optionsFeed);
+
+      setTimeout(drawChart, 15000);
     }
   </script>
 </head>

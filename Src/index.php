1bf48981652b9a4eca52ac6446b24cb1267abe60
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-03-09 23:46:40 +0000

    Add queues table

--- Src/index.php
+++ Src/index.php
@@ -27,12 +27,20 @@ require_once("config.php");
       return JSON.parse(xhr.responseText);
     }
 
+    function loadQueueStats() {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://guilhermebranco.com.br/webhooks/queue.php", false);
+      xhr.send();
+      return JSON.parse(xhr.responseText);
+    }
+
     function drawChart() {
       var response = loadData();
       var dataWebhooks = google.visualization.arrayToDataTable(response["webhooks"]);
       var dataEvents = google.visualization.arrayToDataTable(response["events"]);
       var dataFeed = google.visualization.arrayToDataTable(response["feed"]);
-      var dataTotal = google.visualization.arrayToDataTable([["Hits", "Total"], ["Total", response["total"]]]);
+      var dataTotal = google.visualization.arrayToDataTable([["Hits", "Total"], ["Webhooks", response["total"]]]);
+      var dataFailed = google.visualization.arrayToDataTable([["Hits", "Failed"], ["Failed", response["failed"]]]);
 
       var optionsWebhooks = {
         title: 'GitHub webhooks by date',
@@ -60,12 +68,7 @@ require_once("config.php");
         height: '100%'
       };
 
-      var maxTo = Math.round(response["total"] * 1.1);
-      var maxFrom = Math.round(response["total"] * 0.9);
-      var minTo = Math.round(response["total"] * 0.5);
-
       var optionsTotal = {
-        title: 'GitHub total',
         legend: { position: 'none' },
         showRowNumber: true,
         width: '100%',
@@ -77,30 +80,72 @@ require_once("config.php");
         redFrom: 80000, redTo: 120000
       };
 
+      var optionsFailed = {
+        legend: { position: 'none' },
+        showRowNumber: true,
+        width: '100%',
+        height: '100%',
+        min: 0,
+        max: 1000,
+        greenFrom: 0, greenTo: 50,
+        yellowFrom: 50, yellowTo: 100,
+        redFrom: 100, redTo: 1000
+      };
+
       var lineChart = new google.visualization.LineChart(document.getElementById('line_chart'));
       lineChart.draw(dataWebhooks, optionsWebhooks);
       var pieChart = new google.visualization.PieChart(document.getElementById('pie_chart'));
       pieChart.draw(dataEvents, optionsEvents);
-      var table = new google.visualization.Table(document.getElementById('table_div'));
-      table.draw(dataFeed, optionsFeed);
+      var feed = new google.visualization.Table(document.getElementById('feed'));
+      feed.draw(dataFeed, optionsFeed);
       var guageChart = new google.visualization.Gauge(document.getElementById('guage_chart'));
       guageChart.draw(dataTotal, optionsTotal);
+      var guageChart2 = new google.visualization.Gauge(document.getElementById('guage_chart_2'));
+      guageChart2.draw(dataFailed, optionsFailed);
+      showQueues();
       setTimeout(drawChart, 30000);
+    }
+
+    function showQueues() {
+      var response = loadQueueStats();
+      var dataQueues = google.visualization.arrayToDataTable(response["queues"]);
+
+      var optionsQueues = {
+        title: 'Messages',
+        legend: { position: 'none' },
+        showRowNumber: true,
+        width: '100%',
+        height: '100%'
+      };
+
+      var queues = new google.visualization.Table(document.getElementById('queues'));
+      queues.draw(dataQueues, optionsQueues);
     }
   </script>
 </head>
 
 <body>
   <div id="line_chart" style="width: 73%; height: 450px; float: left;"></div>
-  <img alt=""
-    src="https://github-readme-stats-guibranco.vercel.app/api?username=guibranco&line_height=28&card_width=490&hide_title=true&hide_border=true&show_icons=true&theme=chartreuse-dark&icon_color=7FFF00&include_all_commits=true&count_private=true&show=reviews,discussions_started&count_private=true" />
-  <img alt=""
-    src="https://github-readme-streak-stats-guibranco.vercel.app/?user=guibranco&theme=github-green-purple&fire=FF6600" />
+  <img id="gh_stats" alt="" src="" />
+  <img id="gh_streak" alt="" src="" />
   <div style="clear:both;"></div>
-  <div id="pie_chart" style="width: 73%; height: 300px; float: left;"></div>
+  <div id="pie_chart" style="width: 35%; height: 300px; float: left;"></div>
   <div id="guage_chart" style="width: 20%; height: 300px; float: left;background-color: rgb(13, 17, 23);"></div>
+  <div id="guage_chart_2" style="width: 20%; height: 300px; float: left;background-color: rgb(13, 17, 23);"></div>
+  <div id="queues" style="width:25%; padding-top: 10px; float: left;"></div>
   <div style="clear:both;"></div>
-  <div id="table_div"></div>
+  <div id="feed"></div>
 </body>
+<script>
+  var ghStats = document.getElementById("gh_stats");
+  ghStats.src = "https://github-readme-stats-guibranco.vercel.app/api" +
+    "?username=guibranco&line_height=28&card_width=490&hide_title=true&hide_border=true" +
+    "&show_icons=true&theme=chartreuse-dark&icon_color=7FFF00&include_all_commits=true" +
+    "&count_private=true&show=reviews,discussions_started&count_private=true";
+
+  var ghStreak = document.getElementById("gh_streak");
+  ghStreak.src = "https://github-readme-streak-stats-guibranco.vercel.app/" +
+    "?user=guibranco&theme=github-green-purple&fire=FF6600";
+</script>
 
 </html>

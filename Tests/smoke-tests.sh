f1e4803b0e2a7affd5cc08b03650a7b89cd593bb
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-03-10 17:29:31 +0000

    Fix location

--- Tests/smoke-tests.sh
+++ Tests/smoke-tests.sh
@@ -11,7 +11,7 @@ fi
 RESULT=$(curl -s -o /dev/null -w "%{http_code}" --location "http://localhost:8000/api/v1/logs.php")
 if [ "$RESULT" -ne 401 ]; then
     CONTENT=$(curl --location "http://localhost:8000/api/v1/logs.php")
-    echo "::set-output name=content::$CONTENT"
-    echo "::set-output name=error::true"
+    echo "content=$CONTENT">>"$GITHUB_OUTPUT"
+    echo "error=true">>"$GITHUB_OUTPUT"
     exit 1
 fi

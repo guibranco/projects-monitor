fbccb8149dbc35c6a17fdf3363ce9ac797af2d0c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-03-11 12:17:07 +0000

    Feature/fix database (#64)
    * Fix log
    
    * Fix checks
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in f0b033d according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/64
    
    * Update Logger.php
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/Library/Logger.php
+++ Src/Library/Logger.php
@@ -19,16 +19,17 @@ class Logger
         $stmt = $conn->prepare($sql);
 
         $appId = $applicationId;
-        $class = $data["class"];
-        $method = $data["method"];
-        $file = $data["file"];
-        $line = $data["line"];
-        $message = $data["message"];
-        $stackTrace = $data["stack_trace"];
+
+        $class = isset($data["class"]) ? $data["class"] : "none";
+        $method = isset($data["method"]) ? $data["method"] : "none";
+        $file = isset($data["file"]) ? $data["file"] : "none";
+        $line = isset($data["line"]) ? $data["line"] : "none";
+        $message = isset($data["message"]) ? $data["message"] : "none";
+        $stackTrace = isset($data["stack_trace"]) ? $data["stack_trace"] : "none";
 
         $stmt->bind_param("isssiss", $appId, $class, $method, $file, $line, $message, $stackTrace);
 
-        if(!$stmt->execute()) {
+        if (!$stmt->execute()) {
             return false;
         }
         return true;

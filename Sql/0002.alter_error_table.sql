20b0e3347c7d3831a4b3742159a6fcf99d6169be
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-03-15 20:06:18 +0000

    Create 0002.alter_error_table.sql

---
+++ Sql/0002.alter_error_table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `errors` 
+  ADD `object` text NULL AFTER `line`,
+  ADD `type` varchar(255) NULL AFTER `object`,
+  ADD `args` text NULL AFTER `type`,
+  ADD `details` text NULL AFTER `message`

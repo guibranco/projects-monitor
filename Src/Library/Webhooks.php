72a6f2223103d07fe5a5426ddbd0d0edff4f11f8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-05-28 23:57:00 +0100

    Create Webhooks.php (#145)
    * Create Webhooks.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in b96b222 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/145
    
    * Update Webhooks.php
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

---
+++ Src/Library/Webhooks.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GuiBranco\ProjectsMonitor\Library;
+
+use GuiBranco\Pancake\Request;
+
+class Webhooks
+{
+    private const API_URL = "https://guilhermebranco.com.br/webhooks/api.php";
+
+    private $request;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
+
+    public function getDashboard()
+    {
+        $headers = [
+            "Content-Type: application/json",
+            "Accept: application/json",
+            "Cache-Control: no-cache",
+            "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)"
+        ];
+
+        $response = $this->request->get(API_URL, $headers);
+
+        if ($response->statusCode != 200) {
+            throw new WebhooksException("Error: {$response->body}");
+        }
+
+        return json_decode($response->body);
+    }
+}

3277dba001cd084d229d387b4fea080e6ee68038
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-05-28 22:44:41 +0100

    Update GitHub.php

--- Src/Library/GitHub.php
+++ Src/Library/GitHub.php
@@ -65,7 +65,7 @@ class GitHub
         foreach ($items as $item) {
             $repositoryName = str_replace("https://api.github.com/repos/", "", $item->repository_url);
             $result[] = array(
-                "<a href='" . $item->html_url . "' target='_blank'>" . $item->title . "</a>",
+                "<a href='" . $item->html_url . "' target='_blank'>[#" . $item->number . "] " . $item->title . "</a>",
                 "<a href='https://github.com/" . $repositoryName . "' target='_blank'>" . $repositoryName . "</a>",
                 "<a href='" . $item->user->html_url . "' target='_blank'>" . $item->user->login . "</a>"
             );

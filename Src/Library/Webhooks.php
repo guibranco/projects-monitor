160457f21ac0e5c14ce8f021afc0a61a505bbf0c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-05-30 21:21:40 +0100

    Update Webhooks.php

--- Src/Library/Webhooks.php
+++ Src/Library/Webhooks.php
@@ -8,17 +8,28 @@ class Webhooks
 {
     private const API_URL = "https://guilhermebranco.com.br/webhooks/api.php";
 
+    private $token;
+
     private $request;
 
     public function __construct()
     {
+        global $webhooksApiToken;
+
+        if (!file_exists(__DIR__ . "/../secrets/webhooks.secrets.php")) {
+            throw new WebhooksException("File not found: webhooks.secrets.php");
+        }
+        
+        require_once __DIR__ . "/../secrets/webhooks.secrets.php";
+
+        $this->token = $webhooksApiToken;
         $this->request = new Request();
     }
 
     public function getDashboard()
     {
         $headers = [
-            "Content-Type: application/json",
+            "Authorization: token {$this->token}",
             "Accept: application/json",
             "Cache-Control: no-cache",
             "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)"

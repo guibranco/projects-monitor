25008e7fbd46f53716b4e50c740c671668b6243a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-06 13:37:55 +0100

    Update index.php (#205)

--- Src/index.php
+++ Src/index.php
@@ -16,6 +16,12 @@ $config = new Configuration();
     body {
       background: rgb(13, 17, 23)
     }
+    .gauge {
+      width: 10%;
+      height: 200px;
+      float: left;
+      background-color: white;
+    }
   </style>
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script type="text/javascript" src="static/scripts.js?20240606020300"></script>
@@ -26,16 +32,20 @@ $config = new Configuration();
   <img id="gh_stats" alt="" src="" />
   <img id="gh_streak" alt="" src="" />
   <div style="clear:both;"></div>
-  <div id="gauge_chart_1" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
-  <div id="gauge_chart_2" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
-  <div id="gauge_chart_3" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
-  <div id="gauge_chart_4" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
-  <div id="gauge_chart_5" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
-  <div id="gauge_chart_6" style="width: 15%; height: 300px; float: left;background-color: white;"></div>
+  <div id="gauge_chart_1" class="gauge"></div>
+  <div id="gauge_chart_2" class="gauge"></div>
+  <div id="gauge_chart_3" class="gauge"></div>
+  <div id="gauge_chart_4" class="gauge"></div>
+  <div id="gauge_chart_5" class="gauge"></div>
+  <div id="gauge_chart_6" class="gauge"></div>
+  <div id="gauge_chart_7" class="gauge"></div>
   <div style="clear:both;"></div>
-  <div id="pie_chart_1" style="width: 30%; height: 300px; float: left;"></div>
-  <div id="pie_chart_2" style="width: 30%; height: 300px; float: left;"></div>
-  <div id="queues" style="width: 30%; height: 300px; float: left;background-color: white;"></div>
+  <div style="width: 30%; height: 600px; float: left;">
+    <div id="pie_chart_1" style="width: 100%; height: 300px;"></div>
+    <div id="pie_chart_2" style="width: 100%; height: 300px;"></div>
+  </div>
+  <div id="queues" style="width: 30%; height: 600px; float: left;background-color: white;"></div>
+  <div id="latest_release" style="width: 30%; height: 300px; float: left;background-color: white;"></div>
   <div style="clear:both;"></div>
   <div id="repositories" style="width: 50%; height: 500px; float: left;background-color: white;"></div>
   <div style="width: 50%; height: 500px; float: left;background-color: white;">
@@ -45,8 +55,7 @@ $config = new Configuration();
   <div style="clear:both;"></div>
   <div id="feed" style="width: 50%; float: left;background-color: white;"></div>
   <div style="width: 50%; float: left;background-color: white;">
-    <div id="workflow_runs"></div>
-    <div id="latest_release"></div>
+    <div id="workflow_runs"></div>    
     <div id="pull_requests"></div>
     <div id="bug"></div>
     <div id="triage"></div>

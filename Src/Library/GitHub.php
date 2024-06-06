5f71a7be38f41a012f468747fcdce64c1aa85b73
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-06 21:58:05 +0100

    Luminance

--- Src/Library/GitHub.php
+++ Src/Library/GitHub.php
@@ -65,7 +65,7 @@ class GitHub
         foreach ($items as $item) {
             $repositoryName = str_replace("https://api.github.com/repos/", "", $item->repository_url);
             $labels = implode(" ", array_map(function ($label) {
-                return "<span style='background-color: #" . $label->color . "; color: #" . (Color::luminance($label->color) > 0.5 ? "000" : "fff") . "; padding: 2px; border-radius: 5px;'>" . $label->name . "</span>";
+                return "<span style='background-color: #" . $label->color . "; color: #" . (Color::luminance($label->color) > 90 ? "000" : "fff") . "; padding: 2px; border-radius: 5px; border: 1px solid #000;'>" . $label->name . "</span>";
             }, $item->labels));
             $result[] = array(
                 "<a href='" . $item->html_url . "' target='_blank'>[#" . $item->number . "] " . $item->title . " " . $labels . "</a>",

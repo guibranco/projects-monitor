3fdfac996dd6578a1f398276370ca2a4639b7431
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-11 14:30:17 +0100

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -56,26 +56,26 @@ $config = new Configuration();
     <img id="gh_streak" style="width: 465px; float: left;" alt="GH Streak" src="" />
   </div>
   <div style="clear:both;"></div>
-  <div style="width: 30%; height: 600px; float: left;">
+  <div style="width: 30%; height: 600px; float: left; background-color: white;">
     <div id="pie_chart_1" style="width: 100%; height: 300px;"></div>
     <div id="pie_chart_2" style="width: 100%; height: 300px;"></div>
   </div>
-  <div id="queues" style="width: 30%; height: 600px; float: left;background-color: white;"></div>
+  <div id="queues" style="width: 30%; height: 600px; float: left; background-color: white;"></div>
   <div style="width: 40%; height: 400px; float: left; background-color: white;">
-    <div id="accounts_usage" style="width: 100%; height: 150px;"></div>
-    <div id="latest_release" style="width: 100%; height: 150px; overflow: scroll;"></div>
-    <div id="errorLogFiles" style="width: 100%; height: 100px"></div>
+    <div id="accounts_usage" style="width: 100%; height: 200px;"></div>
+    <div id="latest_release" style="width: 100%; height: 250px;"></div>
+    <div id="errorLogFiles" style="width: 100%; height: 100px;"></div>
   </div>
   <div style="clear:both;"></div>
-  <div id="repositories" style="width: 50%; height: 500px; float: left;background-color: white;"></div>
-  <div id="errorLogMessages" style="width: 50%; height: 500px; float: left;background-color: white;"></div>
+  <div id="repositories" style="width: 50%; height: 500px; float: left; background-color: white;"></div>
+  <div id="errorLogMessages" style="width: 50%; height: 500px; float: left; background-color: white;"></div>
   <div style="clear:both;"></div>
-  <div style="width: 50%; float: left;background-color: white;">
+  <div style="width: 50%; float: left; background-color: white;">
     <div id="feed"></div>
     <div id="healthchecksio"></div>
     <div id="uptimerobot"></div>
   </div>
-  <div style="width: 50%; float: left;background-color: white;">
+  <div style="width: 50%; float: left; background-color: white;">
     <div id="workflow_runs"></div>
     <div id="pull_requests"></div>
     <div id="bug"></div>
@@ -85,7 +85,7 @@ $config = new Configuration();
     <div id="cronjobs"></div>    
   </div>
   <div style="clear:both;"></div>
-  <div id="messages" style="width: 100%; float: left;background-color: white;"></div>
+  <div id="messages" style="width: 100%; float: left; background-color: white;"></div>
   <div style="clear:both;"></div>
 </body>
 

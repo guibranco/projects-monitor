8e68baba2b544728892be4e0b21f55f03ad85cd3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-11 10:05:30 +0100

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -62,9 +62,9 @@ $config = new Configuration();
   </div>
   <div id="queues" style="width: 30%; height: 600px; float: left;background-color: white;"></div>
   <div style="width: 40%; height: 400px; float: left; background-color: white;">
+    <div id="accounts_usage" style="width: 100%; height: 150px;"></div>
     <div id="latest_release" style="width: 100%; height: 150px;"></div>
     <div id="errorLogFiles" style="width: 100%; height: 100px"></div>
-    <div id="accounts_usage" style="width: 100%; height: 150px;"></div>
   </div>
   <div style="clear:both;"></div>
   <div id="repositories" style="width: 50%; height: 500px; float: left;background-color: white;"></div>

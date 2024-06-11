2228fcb5a167351f9e4cea6dafad19c69ad5cc69
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-11 21:15:51 +0100

    Add HTML link

--- Src/Library/Ip2WhoIs.php
+++ Src/Library/Ip2WhoIs.php
@@ -56,7 +56,7 @@ class Ip2WhoIs
             $expireDate = date("d/m/Y", strtotime($response->expire_date));
             $status = preg_replace($pattern, '', $response->status);
             $nameservers = implode(",", $response->nameservers);
-            $data[] = array($domain, $createDate, $expireDate, $response->domain_age, $status, $nameservers);
+            $data[] = array("<a href='https://$domain'>$domain</a>", $createDate, $expireDate, $response->domain_age, $status, $nameservers);
         }
 
         sort($data);

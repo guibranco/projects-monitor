9813e8ad0b5c50b5fae256cca4521bfdadc76af3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-11 14:40:51 +0100

    Update index.php (#256)

--- Src/index.php
+++ Src/index.php
@@ -61,10 +61,10 @@ $config = new Configuration();
     <div id="pie_chart_2" style="width: 100%; height: 300px;"></div>
   </div>
   <div id="queues" style="width: 30%; height: 600px; float: left; background-color: white;"></div>
-  <div style="width: 40%; height: 400px; float: left; background-color: white;">
+  <div style="width: 40%; height: 600px; float: left; background-color: white;">
     <div id="accounts_usage" style="width: 100%; height: 200px;"></div>
     <div id="latest_release" style="width: 100%; height: 250px;"></div>
-    <div id="errorLogFiles" style="width: 100%; height: 100px;"></div>
+    <div id="errorLogFiles" style="width: 100%; height: 150px;"></div>
   </div>
   <div style="clear:both;"></div>
   <div id="repositories" style="width: 50%; height: 500px; float: left; background-color: white;"></div>

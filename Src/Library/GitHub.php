b2b4e550f2796068a2dac4b87112f1d9d270ec78
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-13 12:08:26 +0100

    Update GitHub.php

--- Src/Library/GitHub.php
+++ Src/Library/GitHub.php
@@ -109,13 +109,15 @@ class GitHub
         );
 
         $data = array();
-        $result = $this->getRequest($users, "issue", null, ["WIP", "bug", "triage"]);
+        $result = $this->getRequest($users, "issue");
+        $resultOthers = $this->getRequest($users, "issue", null, ["WIP", "bug", "triage"]);
         $resultWip = $this->getRequest($users, "issue", "WIP");
         $resultBug = $this->getRequest($users, "issue", "bug");
         $resultTriage = $this->getRequest($users, "issue", "triage");
         $resultAssigned = $this->getassignedIssues(array_slice($users, 0, 1)[0], $users);
         $data["total_count"] = $result->total_count;
-        $data["latest"] = $this->mapItems($result->items);
+        $data["latest"] = $this->mapItems($resultOthers->items);
+        $data["others"] $data["latest"];
         $data["wip"] = $this->mapItems($resultWip->items);
         $data["bug"] = $this->mapItems($resultBug->items);
         $data["triage"] = $this->mapItems($resultTriage->items);

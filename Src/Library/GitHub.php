d96c3ec7d2ade5fd5c43cae3f620269a807a9940
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-15 17:50:34 +0100

    Update GitHub.php

--- Src/Library/GitHub.php
+++ Src/Library/GitHub.php
@@ -42,7 +42,7 @@ class GitHub
             return json_decode(file_get_contents($cache));
         }
 
-        $url = self::GITHUB_API_URL . "search/issues?q=" . $queryString;
+        $url = self::GITHUB_API_URL . "search/issues?q=" . urlencode(preg_replace('!\s+!', ' ', "is:open archived:false is:{$queryString}"));
         $response = $this->request->get($url, $this->headers);
 
         if ($response->statusCode != 200) {
@@ -68,7 +68,7 @@ class GitHub
         }
 
         $usersList = implode(" ", array_map(function ($user) { return "user:{$user}"; }, $users));
-        $queryString = urlencode(preg_replace('!\s+!', ' ', "is:open is:{$type} archived:false ${labels} {$labelsRemove} {$usersList}"));
+        $queryString = "{$type} ${labels} {$labelsRemove} {$usersList}";
 
         return $this->getSearch($queryString);
     }
@@ -77,7 +77,7 @@ class GitHub
     {
         $filterString = "{$filter}:{$user}";
         $usersToRemove = implode(" ", array_map(function ($user) { return "-user:{$user}"; }, $usersToExclude));
-        $queryString = urlencode(preg_replace('!\s+!', ' ', "is:open is:{$type} archived:false {$filterString} {$usersToRemove}"));
+        $queryString = "{$type} {$filterString} {$usersToRemove}";
 
         return $this->getSearch($queryString);
     }

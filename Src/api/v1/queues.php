dd17dd2d7caf51338c1ca17028b9f47c4eba5203
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-20 22:35:48 +0100

    Add cache to GitHub calls

--- Src/api/v1/queues.php
+++ Src/api/v1/queues.php
@@ -2,9 +2,9 @@
 
 require_once '../../vendor/autoload.php';
 
-use GuiBranco\ProjectsMonitor\Library\Queue;
+use GuiBranco\ProjectsMonitor\Library\RabbitMq;
 
-$queue = new Queue();
-$data = $queue->getQueueLength();
+$rabbitMq = new RabbitMq();
+$data = $rabbitMq->getQueueLength();
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($data);

71bffde834519939343c9b0fb8ab9aa2bb3280a6
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-07-20 15:52:32 +0100

    Create 0005.alter_charset.sql

---
+++ Sql/0005.alter_charset.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `applications` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `messages` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `schema_version` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

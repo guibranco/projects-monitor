e83e07b18eaa3bcfa750cefcaf5bf26e78ddda5d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-10 18:27:40 +0100

    Update Webhooks.php

--- Src/Library/Webhooks.php
+++ Src/Library/Webhooks.php
@@ -24,6 +24,21 @@ class Webhooks
 
         require_once __DIR__ . "/../secrets/webhooks.secrets.php";
 
+        $timezone = $this->getTimezone();        
+        $this->headers = [
+            "Authorization: token {$this->token}",
+            "Accept: application/json",
+            "Cache-Control: no-cache",
+            "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)",
+            "X-timezone: " . $timezone["timezone"],
+            "X-timezone-offset: " . $timezone["offset"]
+        ];
+        $this->request = new Request();
+        $this->token = $webhooksApiToken;
+    }
+
+    private function getTimezone()
+    {
         $timezone = "Europe/Dublin";
         $offset = "+00:00";
 
@@ -41,17 +56,9 @@ class Webhooks
             $offset = $dateTime->getOffSet() === 3600 ? "+01:00" : "+00:00";
         }
 
-        $this->headers = [
-            "Authorization: token {$this->token}",
-            "Accept: application/json",
-            "Cache-Control: no-cache",
-            "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)",
-            "X-timezone: " . $timezone,
-            "X-timezone-offset: " . $offset
-        ];
-        $this->request = new Request();
-        $this->token = $webhooksApiToken;
+        return array("timezone" => $timezone, "offset" => $offset);
     }
+        
 
     public function getDashboard()
     {

26ff76192b9c8dd7b2ed5963216752f79b5133e7
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-10 18:54:05 +0100

    Update Webhooks.php (#386)
    * Update Webhooks.php
    
    * Update Webhooks.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in 0538f4a according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/386
    
    * Update Webhooks.php
    
    * Update Webhooks.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in e83e07b according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/386
    
    * Update Webhooks.php
    
    ---------
    
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/Library/Webhooks.php
+++ Src/Library/Webhooks.php
@@ -12,6 +12,8 @@ class Webhooks
 
     private $request;
 
+    private $headers;
+
     public function __construct()
     {
         global $webhooksApiToken;
@@ -22,21 +24,45 @@ class Webhooks
 
         require_once __DIR__ . "/../secrets/webhooks.secrets.php";
 
-        $this->token = $webhooksApiToken;
-        $this->request = new Request();
-    }
-
-    public function getDashboard()
-    {
-        $headers = [
+        $timezone = $this->getTimezone();
+        $this->headers = [
             "Authorization: token {$this->token}",
             "Accept: application/json",
             "Cache-Control: no-cache",
-            "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)"
+            "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)",
+            "X-timezone: " . $timezone["timezone"],
+            "X-timezone-offset: " . $timezone["offset"]
         ];
+        $this->request = new Request();
+        $this->token = $webhooksApiToken;
+    }
 
-        $response = $this->request->get(self::API_URL, $headers);
+    private function getTimezone()
+    {
+        $timezone = "Europe/Dublin";
+        $offset = "+00:00";
 
+        if (isset($_COOKIE["timezone"])) {
+            $timezone = strtolower($_COOKIE["timezone"]) === "europe/london"
+                ? "Europe/Dublin"
+                : $_COOKIE["timezone"];
+        }
+
+        if (isset($_COOKIE["offset"])) {
+            $offset = $_COOKIE["offset"];
+        } else {
+            $datetimezone = new \DateTimeZone($timezone);
+            $dateTime = new \DateTime("now", $timezone);
+            $offset = $dateTime->getOffset() === 3600 ? "+01:00" : "+00:00";
+        }
+
+        return array("timezone" => $timezone, "offset" => $offset);
+    }
+
+
+    public function getDashboard()
+    {
+        $response = $this->request->get(self::API_URL, $this->headers);
         if ($response->statusCode != 200) {
             $error = $response->statusCode == -1 ? $response->error : $response->body;
             throw new RequestException("Code: {$response->statusCode} - Error: {$error}");

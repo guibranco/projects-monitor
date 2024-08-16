f74e723aab450b20869842713d10a0da547d38d3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-16 01:10:34 +0100

    Update messages.php (#412)
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- Src/api/v1/messages.php
+++ Src/api/v1/messages.php
@@ -4,19 +4,15 @@ require_once '../../vendor/autoload.php';
 
 use GuiBranco\ProjectsMonitor\Library\Logger;
 
-$quantity = isset($_GET["quantity"]) ? intval($_GET["quantity"]) : 100;
-
 $log = new Logger();
 $total = $log->getTotal();
 $byApplications = $log->getTotalByApplications();
 $grouped = $log->getGroupedMessages();
-$messages = $log->showLastMessages($quantity);
 
 $result = [
     "total" => $total,
     "byApplications" => $byApplications,
-    "grouped" => $grouped,
-    "messages" => $messages
+    "grouped" => $grouped
 ];
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($result);

18652bf8ba8f4384557b307f6553efcf01b1360e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-17 03:06:02 +0100

    Update Webhooks.php

--- Src/Library/Webhooks.php
+++ Src/Library/Webhooks.php
@@ -3,6 +3,7 @@
 namespace GuiBranco\ProjectsMonitor\Library;
 
 use GuiBranco\Pancake\Request;
+use GuiBranco\ProjectsMonitor\Library\TimeZone;
 
 class Webhooks
 {
@@ -22,39 +23,17 @@ class Webhooks
 
         require_once __DIR__ . "/../secrets/webhooks.secrets.php";
 
-        $timezone = $this->getTimezone();
+        $timeZone = new TimeZone();
         $this->headers = [
             "Authorization: token {$webhooksApiToken}",
             "Accept: application/json",
             "Cache-Control: no-cache",
             "User-Agent: ProjectsMonitor/1.0 (+https://github.com/guibranco/projects-monitor)",
-            "X-timezone: {$timezone["timezone"]}",
-            "X-timezone-offset: {$timezone["offset"]}"
+            "X-timezone: {$timeZone->getTimeZone()}",
+            "X-timezone-offset: {$timeZone->getOffset()}"
         ];
         $this->request = new Request();
-    }
-
-    private function getTimezone()
-    {
-        $timezone = "Europe/Dublin";
-        $offset = "+00:00";
-
-        if (isset($_COOKIE["timezone"])) {
-            $timezone = strtolower($_COOKIE["timezone"]) === "europe/london"
-                ? "Europe/Dublin"
-                : $_COOKIE["timezone"];
-        }
-
-        if (isset($_COOKIE["offset"])) {
-            $offset = $_COOKIE["offset"];
-        } else {
-            $datetimezone = new \DateTimeZone($timezone);
-            $dateTime = new \DateTime("now", $datetimezone);
-            $offset = $dateTime->getOffset() === 3600 ? "+01:00" : "+00:00";
-        }
-
-        return array("timezone" => $timezone, "offset" => $offset);
-    }
+    }   
 
     public function getDashboard()
     {

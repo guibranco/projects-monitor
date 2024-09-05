ebb505ae8bd17fb355e2b185ab471e276883e49d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-05 22:30:17 +0100

    Update 0007.create_errors_table.sql

--- Sql/0007.create_errors_table.sql
+++ Sql/0007.create_errors_table.sql
@@ -3,12 +3,18 @@ DROP TABLE IF EXISTS `errors`;
 CREATE TABLE
     `errors` (
         `id` int NOT NULL AUTO_INCREMENT,
-        `date` DATETIME NOT NULL,
         `error_log_path` varchar(255) NOT NULL,
+        `date` datetime NOT NULL,
+        `error` text NOT NULL,
+        `error_multiline` text NOT NULL,
         `file` varchar(255) NOT NULL,
-        `line` int (11) NOT NULL,        
-        `message` text NOT NULL,
-        `details` text NULL,
+        `line` int (11) NOT NULL,
+        `stack_trace` text NULL,
+        `stack_trace_details` text NULL,
+        `repository` varchar(255) NULL,
+        `issue_number` int NULL,
+        `issue_created_at` timestamp NULL,
         `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+        `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
         PRIMARY KEY (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;

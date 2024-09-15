878a918d3a13e1452d4ba4353bdbb71c514a36b5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-16 00:46:29 +0100

    Refactor Webhooks.php for Enhanced API Request Management (#514)
    * Update Webhooks.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in 2960cee according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/514
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- Src/Library/Webhooks.php
+++ Src/Library/Webhooks.php
@@ -38,10 +38,26 @@ class Webhooks
         $this->request = new Request();
     }
 
-    public function getDashboard()
+    private function doRequest($endpoint, $method, $expectedStatusCode, $data = null)
     {
-        $response = $this->request->get("{$this->apiUrl}github", $this->headers);
-        if ($response->statusCode === 200) {
+        $response = null;
+        $method = strtolower($method);
+        switch ($method) {
+            case "get":
+                $response = $this->request->get("{$this->apiUrl}{$endpoint}", $this->headers);
+                break;
+            case "post":
+                $response = $this->request->post("{$this->apiUrl}{$endpoint}", json_encode($data), $this->headers);
+                break;
+            case "delete":
+                $response = $this->request->delete("{$this->apiUrl}{$endpoint}", $this->headers);
+                break;
+            default:
+                throw new RequestException("Method not mapped: {$method}");
+                break;
+        }
+
+        if ($response->statusCode === $expectedStatusCode) {
             return json_decode($response->body);
         }
 
@@ -49,14 +65,23 @@ class Webhooks
         throw new RequestException("Code: {$response->statusCode} - Error: {$error}");
     }
 
+    public function getDashboard()
+    {
+        return $this->doRequest("github", "get", 200);
+    }
+
     public function getWebhook($sequence)
     {
-        $response = $this->request->get("{$this->apiUrl}github/{$sequence}", $this->headers);
-        if ($response->statusCode === 200) {
-            return json_decode($response->body);
-        }
+        return $this->doRequest("github/{$sequence}", "get", 200);
+    }
 
-        $error = $response->statusCode == -1 ? $response->error : $response->body;
-        throw new RequestException("Code: {$response->statusCode} - Error: {$error}");
+    public function requestRerun($sequence)
+    {
+        return $this->doRequest("github/workflow", "post", 201, array("sequence", $sequence));
+    }
+
+    public function requestDelete($sequence)
+    {
+        return $this->doRequest("github/workflow/{$sequence}", "delete", 202);
     }
 }

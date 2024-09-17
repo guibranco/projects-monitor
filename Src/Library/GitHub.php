a91b1217dd2c90139c2892cce4778718c4cf1304
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-18 00:16:50 +0100

    Enhance Markdown Handling in GitHub.php (#528)
    * Update GitHub.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in 57b59e2 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/528
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/Library/GitHub.php
+++ Src/Library/GitHub.php
@@ -119,8 +119,6 @@ class GitHub
 
         $result = array();
 
-        $mkd = Markdown::new();
-
         foreach ($items as $item) {
             $repositoryName = str_replace("https://api.github.com/repos/", "", $item->repository_url);
             $labelsJson = $item->labels;
@@ -130,6 +128,7 @@ class GitHub
             $labels = implode(" ", array_map(function ($label) {
                 return "<span style='background-color: #" . $label->color . ";color: #" . (Color::luminance($label->color) > 120 ? "000" : "fff") . ";padding: 0 7px;border-radius: 24px;border: 1px solid #000;line-height: 21px;text-wrap:nowrap;'>" . $label->name . "</span>";
             }, $labelsJson));
+            $mkd = Markdown::new();
             $mkd->setContent($item->title);
             $title = $mkd->toHtml();
             $colorNumber = Color::generateColorFromText($repositoryName);

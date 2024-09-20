59577e5a39434da95d634dcfc4028b1a5ad2abba
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-20 01:05:49 +0100

    Update wireguard.php

--- Src/api/v1/wireguard.php
+++ Src/api/v1/wireguard.php
@@ -5,7 +5,8 @@ require_once '../../vendor/autoload.php';
 use GuiBranco\ProjectsMonitor\Library\SSH;
 
 $ssh = new SSH();
-$data = $ssh->getWireGuardConnections();
+$data = array();
+$data["wireguard"] = $ssh->getWireGuardConnections();
 
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($data);

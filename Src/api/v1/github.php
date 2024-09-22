0d0a622305576c741d43dfcdc4257e90d618a659
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-23 00:46:50 +0100

    Update github.php

--- Src/api/v1/github.php
+++ Src/api/v1/github.php
@@ -9,6 +9,7 @@ $data["issues"] = $github->getIssues();
 $data["pull_requests"] = $github->getPullRequests();
 $data["latest_release"] = $github->getLatestReleaseOfBancosBrasileiros();
 $data["accounts_usage"] = $github->getAccountUsage();
+$data["api_usage"] =  $github->getApiUsage();
 
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($data);

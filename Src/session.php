622aed62663e88d41dc2b4653ea04bd2b2a2c5e9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 19:19:56 +0000

    Update session.php

--- Src/session.php
+++ Src/session.php
@@ -10,5 +10,15 @@ if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.cookie_secure', 1);
     ini_set('session.use_strict_mode', 1);
     ini_set('session.cookie_samesite', 'Lax');
+    $expires = 60;
+    $cookie_lifetime = 604800;
+    session_set_cookie_params([
+        'lifetime' => $cookie_lifetime,
+        'path' => '/',
+        'domain' => $_SERVER['HTTP_HOST'],
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'Strict'
+    ]);
     session_start();
 }

81126e8c9061cbdbc48312a709a808ff6c48eb91
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 02:10:41 +0000

    Update session_validator.php

--- Src/api/v1/session_validator.php
+++ Src/api/v1/session_validator.php
@@ -1,16 +1,16 @@
 <?php
 
+if (headers_sent($file, $line)) {
+    error_log("Headers already sent in $file:$line");
+    exit(1);
+}
+
 if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.cookie_httponly', 1);
     ini_set('session.cookie_secure', 1);
     ini_set('session.use_strict_mode', 1);
     ini_set('session.cookie_samesite', 'Lax');
     session_start();
-}
-
-if (headers_sent($file, $line)) {
-    error_log("Headers already sent in $file:$line");
-    exit(1);
 }
 
 header('X-Content-Type-Options: nosniff');

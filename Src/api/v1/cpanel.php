58dfa3f203b20ee2d54bce81d911783ec18c5f62
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 02:03:01 +0000

    Update cpanel.php

--- Src/api/v1/cpanel.php
+++ Src/api/v1/cpanel.php
@@ -1,29 +1,9 @@
 <?php
 
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.use_strict_mode', 1);
-ini_set('session.cookie_samesite', 'Lax');
-session_start();
-
+require_once 'session_validator.php';
 require_once '../../vendor/autoload.php';
 
 use GuiBranco\ProjectsMonitor\Library\CPanel;
-
-if (!isset($_SESSION['last_activity']) || (time() - $_SESSION['last_activity'] > 1800)) {
-    session_unset();
-    session_destroy();
-    header('Location: login.php');
-    exit;
-}
-$_SESSION['last_activity'] = time();
-
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-    header('Pragma: no-cache');
-    exit;
-}
 
 $cPanel = new CPanel();
 $logMessages = $cPanel->getErrorLogMessages();

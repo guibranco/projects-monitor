e91afc7c7affef52d50c23bcacaaab178cdfa461
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 19:15:42 +0000

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -1,10 +1,5 @@
 <?php
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.use_strict_mode', 1);
-ini_set('session.cookie_samesite', 'Lax');
-session_start();
-
+require_once 'session.php';
 require_once 'vendor/autoload.php';
 
 use GuiBranco\ProjectsMonitor\Library\Configuration;

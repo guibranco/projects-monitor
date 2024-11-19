83927bbacdc0d50f9238e5dcc7d03b5c383cd4c4
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 02:16:00 +0000

    Update logout.php

--- Src/logout.php
+++ Src/logout.php
@@ -1,26 +1,19 @@
 <?php
 
-<?php
-
 session_start();
-// Clear all session variables
 $_SESSION = array();
 
-// Delete the session cookie
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 3600, '/');
 }
 
 session_destroy();
-// Ensure no output has been sent
 if (!headers_sent()) {
-    // Use absolute path and add CSRF token
     $token = bin2hex(random_bytes(32));
     $_SESSION['csrf_token'] = $token;
     header('Location: /login.php?token=' . urlencode($token));
     exit;
 } else {
-    // Fallback for when headers are already sent
     echo '<script>window.location.href = "/login.php";</script>';
     echo 'If you are not redirected, please <a href="/login.php">click here</a>.';
     exit;

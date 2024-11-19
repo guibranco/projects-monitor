57161b061f42d5a7ba520c4c75fa8ebfa1ec6c51
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 01:56:53 +0000

    Update session_validator.php

--- Src/api/v1/session_validator.php
+++ Src/api/v1/session_validator.php
@@ -1,6 +1,10 @@
 <?php
 
 if (session_status() === PHP_SESSION_NONE) {
+    ini_set('session.cookie_httponly', 1);
+    ini_set('session.cookie_secure', 1);
+    ini_set('session.use_strict_mode', 1);
+    ini_set('session.cookie_samesite', 'Lax');
     session_start();
 }
 

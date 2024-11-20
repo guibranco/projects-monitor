db94a85ee026d2fc2d9a77e0f5a9044a32b0f785
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-20 00:59:58 +0000

    Update domains.php

--- Src/api/v1/domains.php
+++ Src/api/v1/domains.php
@@ -8,6 +8,4 @@ use GuiBranco\ProjectsMonitor\Library\Ip2WhoIs;
 $ip2WhoIs = new Ip2WhoIs();
 $data = array();
 $data["domains"] = $ip2WhoIs->getDomainValidity();
-
-header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($data);

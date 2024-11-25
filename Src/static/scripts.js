912f22b9c4e374f5558a5334f30c70c44fa3d55b
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-25 01:28:00 +0000

    Update scripts.js (#687)

--- Src/static/scripts.js
+++ Src/static/scripts.js
@@ -63,6 +63,8 @@ class FeedState {
     }
 }
 
+const feedState = new FeedState();
+
 function updateFeedPreference(toggle) {
     if (!toggle || typeof toggle.checked !== 'boolean') {
         console.error('Invalid toggle parameter');

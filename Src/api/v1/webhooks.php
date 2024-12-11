015fa1a2bea8c6971ae090e3b800a5b8d6b31851
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-11 12:43:10 +0000

    Guibranco patch 1 (#711)
    * Update webhooks.php
    
    * Update Webhooks.php
    
    * Update Src/api/v1/webhooks.php
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update webhooks.php
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Src/api/v1/webhooks.php
+++ Src/api/v1/webhooks.php
@@ -10,6 +10,19 @@ $feedOptionsFilter = isset($_GET["feedOptionsFilter"]) && in_array($_GET["feedOp
     ? $_GET["feedOptionsFilter"]
     : "all";
 
+$workflowsLimiterEnabled = filter_var(
+    $_GET["workflowsLimiterEnabled"] ?? false,
+    FILTER_VALIDATE_BOOLEAN
+);
+$maxLimit = 10000;
+$workflowsLimiterQuantity = $workflowsLimiterEnabled
+    ? filter_var(
+        $_GET["workflowsLimiterQuantity"] ?? 0,
+        FILTER_VALIDATE_INT,
+        ["options" => ["min_range" => 1, "max_range" => $maxLimit]]
+    ) ?: 0
+    : 0;
+
 $webhooks = new Webhooks();
-$data = $webhooks->getDashboard($feedOptionsFilter);
+$data = $webhooks->getDashboard($feedOptionsFilter, $workflowsLimiterQuantity);
 echo json_encode($data);

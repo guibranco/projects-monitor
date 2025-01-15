60d639910ae5c883b468fb5a090680037a80b6f0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-15 22:52:42 +0000

    Update github.php

--- Src/api/v1/github.php
+++ Src/api/v1/github.php
@@ -6,10 +6,10 @@ require_once '../../vendor/autoload.php';
 use GuiBranco\ProjectsMonitor\Library\GitHub;
 
 $github = new GitHub();
-$data["api_usage"] =  $github->getApiUsage();
-$usage = $github->getAccountUsage();
-$data["accounts_usage"] = $usage["data"];
-$data["account_usage_core"] = $usage["core"];
+$apiUsage = $github->getApiUsage();;
+$data["api_usage"] = $apiUsage["data"];
+$data["api_usage_core"] = $apiUsage["core"];
+$data["accounts_usage"] = $github->getAccountUsage();
 $data["issues"] = $github->getIssues();
 $data["pull_requests"] = $github->getPullRequests();
 $data["latest_release"] = $github->getLatestReleaseOfBancosBrasileiros();

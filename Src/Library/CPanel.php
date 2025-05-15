83cc65d490a646a3712cb48d457e99189c4bc5a2
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-05-15 11:15:43 +0100

    Update CPanel.php

--- Src/Library/CPanel.php
+++ Src/Library/CPanel.php
@@ -405,27 +405,28 @@ class CPanel
         return $inboxMessagesCount;
     }
 
- /**
- * Deletes an error log file by filename.
- *
- * @param string $directory The directory where ther error_log file to delete is
- * @return bool True if the file was successfully deleted, false otherwise
- */
-public function deleteErrorLogFile($directory): bool
-{
-    $fullFilename = "/home/{$this->username}/{$directory}/error_log";    
-    $parameters = array(
-        "cpanel_jsonapi_module" => "Fileman",
-        "cpanel_jsonapi_func" => "fileop",
-        "cpanel_jsonapi_apiversion" => "2",
-        "op" => "trash",
-        "sourcefiles" => $fullFilename
-    );
-   
-    try {
-        $response = $this->getRequest("json-api", "cpanel", $parameters);
-        return isset($response->cpanelresult->data->result) === true && $response->cpanelresult->data->result === 1;
-    } catch (RequestException $e) {        
-        return false;
+     /**
+     * Deletes an error log file by filename.
+     *
+     * @param string $directory The directory where ther error_log file to delete is
+     * @return bool True if the file was successfully deleted, false otherwise
+     */
+    public function deleteErrorLogFile($directory): bool
+    {
+        $fullFilename = "/home/{$this->username}/{$directory}/error_log";    
+        $parameters = array(
+            "cpanel_jsonapi_module" => "Fileman",
+            "cpanel_jsonapi_func" => "fileop",
+            "cpanel_jsonapi_apiversion" => "2",
+            "op" => "trash",
+            "sourcefiles" => $fullFilename
+        );
+       
+        try {
+            $response = $this->getRequest("json-api", "cpanel", $parameters);
+            return isset($response->cpanelresult->data->result) === true && $response->cpanelresult->data->result === 1;
+        } catch (RequestException $e) {        
+            return false;
+        }
     }
 }

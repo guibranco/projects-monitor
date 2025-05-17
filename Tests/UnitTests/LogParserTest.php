d6775b7e91f04426f84b17bbc01ed42062a38f49
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-05-17 21:35:50 +0100

    Update LogParserTest.php

--- Tests/UnitTests/LogParserTest.php
+++ Tests/UnitTests/LogParserTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use GuiBranco\ProjectsMonitor\Library\LogParser;
-use InvalidArgumentException;
 
 final class LogParserTest extends TestCase
 {

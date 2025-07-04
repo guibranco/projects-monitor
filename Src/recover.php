8ddc6e650c46c13fbd02839dbe570552b3c0d3e0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-07-04 11:00:08 +0100

    Improve dashboard page layout (#938)
    * Improve dashboard page layout
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in dce6510 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/projects-monitor/pull/938
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/recover.php
+++ Src/recover.php
@@ -149,6 +149,7 @@ $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="static/styles-public.css?<?php echo filemtime("static/styles-public.css"); ?>">
 </head>
 
 <body class="bg-light">

f0da716f75188d6e2a953e187dcf65372a6fb7f1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-05-29 23:21:01 +0100

    chore: update Swagger UI library to a more secure source
    Replace the Swagger UI library links with those from the
    cdnjs.cloudflare.com source. This change includes the addition
    of integrity attributes for both the CSS and JS files to enhance
    security by enabling Subresource Integrity (SRI). The crossorigin
    and referrerpolicy attributes are also added to further secure the
    loading of these resources, preventing potential vulnerabilities
    from external sources.

--- Src/swagger.php
+++ Src/swagger.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['user_id'])) {
     <title>Projects Monitor | API Docs</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.1/swagger-ui.css" integrity="sha512-eZpfl9qlKnbDvlJ2brfdx3nhlP1FMsA23w65motxKdYsUcfMcdO2bcLPr7mXhvyzmDZwuzYCJKrl/sEo1ditVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         body { margin: 0; background: #1a1a2e; }
         .swagger-ui .topbar { background-color: #16213e; }
@@ -56,7 +56,7 @@ if (!isset($_SESSION['user_id'])) {
     </div>
     <div id="swagger-ui"></div>
 
-    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.1/swagger-ui-bundle.js" integrity="sha512-iDdi6WwSTimAFh1NhPyFZpHJWr16m/PupHztElqQL+gQoQDavUATsP9hcvgs9Yci+EUA//WvZYZsCTDhRDKc3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
         SwaggerUIBundle({
             url: 'openapi.php',
